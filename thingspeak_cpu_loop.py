8bb6ce3c9f2ec6062a723e2ec83031031dafc4d0
Nicholas Wilde <ncwilde43@gmail.com>
2014-07-10 07:50:32 -0700

    Update thingspeak_cpu_loop.py
    Added cpu_temp for field3 and Python header.

--- thingspeak_cpu_loop.py
+++ thingspeak_cpu_loop.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import httplib, urllib
 from time import localtime, strftime
 # download from http://code.google.com/p/psutil/
@@ -6,8 +8,9 @@ import time
 
 def doit():
 	cpu_pc = psutil.cpu_percent()
-	mem_avail_mb = psutil.avail_phymem()/1000000	
-	params = urllib.urlencode({'field1': cpu_pc, 'field2': mem_avail_mb,'key':'YOURKEYHERE'})
+	mem_avail_mb = psutil.avail_phymem()/1000000
+	cpu_temp = = round(int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3,1)
+	params = urllib.urlencode({'field1': cpu_pc, 'field2': mem_avail_mb, 'field3': cpu_temp,'key':'YOURKEYHERE'})
 	headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
 	conn = httplib.HTTPConnection("api.thingspeak.com:80")
 	
@@ -16,6 +19,7 @@ def doit():
 		response = conn.getresponse()
 		print cpu_pc
 		print mem_avail_mb
+		print cpu_temp
 		print strftime("%a, %d %b %Y %H:%M:%S", localtime())
 		print response.status, response.reason
 		data = response.read()
